ddbebeed01dcd33962b04eccdc1a11c8f4fd2d1a
Tiago Pereira <tiago.peng@gmail.com>
2019-05-26 18:35:12 -0300

    Cenario, esquema de cenario e contexto

---
+++ cucumber/tests/features/step_definitions/cenario.rb
@@ -0,0 +1,22 @@
+
+Dado("que eu tenha {int} laranjas") do |valor1|
+  @laranjas = valor1
+end
+
+Quando("eu como {int} laranjas") do |valor2|
+  @comer = valor2
+  @resultado = @laranjas - @comer
+end
+
+Entao("eu vejo quantas laranjas sobraram") do
+  expect(@resultado).to eql 8
+end
+
+Quando("eu compro {int} laranjas") do |valor3|
+  @compro = valor3
+  @resultado = @laranjas + @compro
+end
+
+Entao("eu vejo quantas laranjas eu tenho") do
+  expect(@resultado).to eql 15
+end
