96ca62bf9003fec40fcaac2d12dfa7702c8ecb78
Tiago Pereira <tiago.peng@gmail.com>
2019-06-03 23:16:19 -0300

    Site Prism - primeiro POM

---
+++ SITEPRISMTESTE/tests/features/step_definitions/acessando_site.rb
@@ -0,0 +1,7 @@
+Quando("acesso a url") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então("verifico se estou na página inicial") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
